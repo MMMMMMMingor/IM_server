428dedfc87e69d6191e1e83d973b4aee48aa89f9
firefly <531109985@qq.com>
2020-04-07 20:58:27 +0800

    update history msg

--- src/server/handler/in_bound_handler.cpp
+++ src/server/handler/in_bound_handler.cpp
@@ -9,6 +9,17 @@
  * @param in_message
  * @return 是否进行下一个handler
  */
+void signalHandler( int signum )
+{
+    std::cout << "Interrupt signal (" << signum << ") received.\n";
+
+    //清理并关闭
+    //终止程序
+
+   // exit(signum);
+}
+
+
 void in_bound_handler(Context &ctx, im_message::Message &in_message) {
     using namespace google::protobuf::io;
 
@@ -17,43 +28,45 @@ void in_bound_handler(Context &ctx, im_message::Message &in_message) {
     ZeroCopyInputStream *raw_input = new FileInputStream(fd);
     auto coded_input = new CodedInputStream(raw_input);
 
-    uint32_t len;
-    bool success1{false};
-    try {
-        success1 = coded_input->ReadVarint32(&len);
-    }catch(...) {
-        std::cout << "catch1\n";
-        if (!success1) {
-            LOG_F(ERROR, "read error fail");
-//        unsigned long long session_id = ctx.session_pool.find_session_id_by_fd(ctx.epoll_fd);
-//        std::cout << session_id << std::endl;
-//        if(session_id != -1) ctx.session_pool.remove_session(session_id);
+    uint32_t len{0};
+    bool success1
+            = coded_input->ReadVarint32(&len);
 
-            // 如果服务器异常关闭， 则删除直接算 logout
+    signal(SIGSEGV, signalHandler);
 
-            return;
+    if (!success1) {
+        LOG_F(ERROR, "read error fail");
+        //不让后面的session进行了
+        ctx.channel->terminate();
+        if(unsigned  long long i = ctx.session_pool.find_session_id_by_fd(fd) != -1){
+            std::cout << "logined\n";
+            ctx.session_pool.remove_session(i);
         }
+        // 如果服务器异常关闭， 则删除直接算 logout
         return;
     }
+
 
     char *buf = new char[len];
     bool success;
     try {
-       success = coded_input->ReadRaw(buf, len);
-    }catch(...){
+        success = coded_input->ReadRaw(buf, len);
+    } catch (...) {
         std::cout << "catch2\n";
         return;
     }
-    in_message.ParseFromArray(buf, len);
-    //      LOG_F(INFO, "%s", response.DebugString().c_str() );
+    in_message.
+            ParseFromArray(buf, len
+    );
+//      LOG_F(INFO, "%s", response.DebugString().c_str() );
 
-    // TODO 未解决半包问题
+// TODO 未解决半包问题
     if (!success)
         LOG_F(ERROR, "read message fail");
 
     ctx.need_recv_message = false; // 读取数据完成
 
-    delete[] buf;
+    delete[]            buf;
     delete coded_input;
     delete raw_input;
 }
